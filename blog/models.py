fc27aa07accbdc54006a29f9149211bf7b0b1a46
Emir <klerni89@gmail.com>
2022-05-04 20:03:21 +0600

    bd connected

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    creation_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import blog_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog_list/', blog_list, name='blog_list'),
 ]
